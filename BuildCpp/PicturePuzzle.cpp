0603765de9feda428b257b121381289b4039a279
TranHuyThinh <tranhuythinh97@gmail.com>
2020-09-29 16:21:06 +0700

    Deeper solving method
    Changed old solving function
    Added estimation for deeper cost calculation

--- BuildCpp/PicturePuzzle.cpp
+++ BuildCpp/PicturePuzzle.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
-#include <windows.h>
+#define MAX 500
 using namespace std;
 
 int n = 9; //total elements
 int d = 3; //elements on a row
-int picture[] = {1,2,3,4,0,5,6,7,8};
+int picture[] = {1,2,3,4,0,5,6,7,8}; //Picture 3x3
 int picKey;
-int puzzle[] = {4,3,0,7,1,5,2,6,8}; //unable to solve: 435710268
+/*puzzle forging steps
+123405678->103425678->013425678->413025678->413205678->413275608
+->413275068->413075268->413705268->403715268->430715268->435710268
+->435701268->435761208->435761280->435760281->435706281*/
+int puzzle[] = {4,3,5,7,6,1,2,8,0};
 int moves[] = {1,-1,d,-d};
 int costs[] = {0,0,0,0};
-int trace[200];
+int trace[MAX];
 int currentTrace = 0;
-
+/* Results:
+Depth 1 step: couldn't solve 435710268,435760281,435706281 (solved 13)
+Depth 5 steps: solved until 435760281 (solved 14)
+*/
 void printPicture(int *pic){
 	for(int i=0; i<n; i++){
 		cout<<pic[i];
@@ -27,7 +34,7 @@ int getKey(int *pic){
 
 int* tryStep(int *puzzle, int dir){
 	int i=0;
-	while(i<n && puzzle[i]!=0) i++;
+	while(puzzle[i]!=0) i++;
 	if(dir==-1 && (i+d)%d==0) return NULL; //piece 0 is first in row can't move backward
 	if(dir==1 && (i+1)%d==0) return NULL; //piece 0 is last in row can't move forward
 	if((i+dir)>=0 && (i+dir)<n){
@@ -46,7 +53,7 @@ int getCost(int *puzzle, int step){
 int getMin(int a, int b){
 	if(a<0) return b;
 	if(b<0) return a;
-	else return(a<b)?a:b;
+	return(a<b)?a:b;
 }
 
 bool hadTrace(int key){
@@ -55,16 +62,45 @@ bool hadTrace(int key){
 	return(trace[i]!=key)?false:true;
 }
 
+int estimateCost(int *puzzle, int currentStep, int estimated, int maxSteps){
+	int c[] = {0,0,0,0};
+	for(int i=0; i<4; i++){
+		if(tryStep(puzzle, moves[i])!=NULL){
+		//if step is valid and is not in traces
+			if(!hadTrace(getKey(puzzle))){
+				currentTrace += 1;
+				trace[currentTrace] = getKey(puzzle);
+				if(estimated<maxSteps) c[i] = getCost(puzzle, currentStep) 
+					+ estimateCost(puzzle, currentStep+1, estimated+1, maxSteps);
+				else c[i] = getCost(puzzle, currentStep);
+				//remove trace
+				trace[currentTrace] = 0;
+				currentTrace -= 1;
+			}
+			else c[i] = -1;
+			tryStep(puzzle, -moves[i]); //undo step
+		}
+		else c[i] = -1;
+	}
+	return getMin(c[0],getMin(c[1],getMin(c[2],c[3])));
+}
+
 void solvePuzzle(int* puzzle){
 	int step = 0;
-	while(getKey(puzzle)!=getKey(picture) && 
-			trace[currentTrace-1] != trace[currentTrace-2]){ //stop if traces duplicated
+	while(getKey(puzzle)!=picKey && currentTrace<MAX &&
+			trace[currentTrace] != trace[currentTrace-1]){ //stop if traces duplicated
 		for(int i=0; i<4; i++){
-			if(tryStep(puzzle, moves[i])!=NULL){
-			//if step is valid and is not in traces
-				if(!hadTrace(getKey(puzzle))) costs[i] = getCost(puzzle, step);
+			if(tryStep(puzzle, moves[i])!= NULL){
+				if(!hadTrace(getKey(puzzle))){
+					currentTrace += 1;
+					trace[currentTrace] = getKey(puzzle);
+					//remove estimateCost if used for depth = 1
+					costs[i] = getCost(puzzle,step)+estimateCost(puzzle,step+1,1,5);
+					trace[currentTrace] = 0;
+					currentTrace -= 1;
+				}
 				else costs[i] = -1;
-				tryStep(puzzle, -moves[i]); //undo step
+				tryStep(puzzle, -moves[i]);
 			}
 			else costs[i] = -1;
 		}
@@ -73,11 +109,10 @@ void solvePuzzle(int* puzzle){
 		while(i<4 && min != costs[i]) i++;
 		tryStep(puzzle, moves[i]); //perform min cost step
 		step += 1;
-		trace[currentTrace] = getKey(puzzle); //add trace
 		currentTrace += 1;
+		trace[currentTrace] = getKey(puzzle); //add trace
 		printPicture(puzzle);
 		cout<<"----"<<endl;
-		Sleep(1000);
 	}
 	if(getKey(puzzle)==getKey(picture)) cout<<"Solved after "<<step<<" moves";
 	else cout<<"Could not solve after "<<step<<" moves";
@@ -91,7 +126,6 @@ int main(){
 	cout<<endl<<"Puzzle:"<<endl;
 	printPicture(puzzle);
 	trace[currentTrace] = getKey(puzzle); //first trace is puzzle key
-	currentTrace += 1;
 	//---------------------------------
 	cout<<endl<<"Solving puzzle:"<<endl;
 	solvePuzzle(puzzle);
