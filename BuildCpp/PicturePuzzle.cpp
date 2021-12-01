ac71a9f251542eaec6386f671ade000f512b46d7
TranHuyThinh <tranhuythinh97@gmail.com>
2021-12-01 09:38:39 +0700

    recursion fix and minor optimisation
    added backtrack when puzzle is solved = fixed trace lost at the end of solving process
    avoid moving backward from last step in solving process

--- BuildCpp/PicturePuzzle.cpp
+++ BuildCpp/PicturePuzzle.cpp
@@ -1,28 +1,45 @@
 #include <iostream>
-#define MAX 500
+#include <math.h>
+#define MAX 1024
 using namespace std;
 
 int n = 9; //total elements
 int d = 3; //elements on a row
-int picture[] = {1,2,3,4,0,5,6,7,8}; //Picture 3x3
+int picture[9] = {1,2,3,4,0,5,6,7,8}; //Picture 3x3
 int picKey;
-/*puzzle forging steps
+/*puzzle making progress
 123405678->103425678->013425678->413025678->413205678->413275608
 ->413275068->413075268->413705268->403715268->430715268->435710268
 ->435701268->435761208->435761280->435760281->435706281*/
-int puzzle[] = {4,3,5,7,6,1,2,8,0};
-int moves[] = {1,-1,d,-d};
-int costs[] = {0,0,0,0};
+int puzzle[9] = {4,3,5,7,6,1,2,8,0};
+int moves[4] = {1,-1,d,-d};
+int costs[4] = {0,0,0,0};
 int trace[MAX];
 int currentTrace = 0;
 /* Results:
 Depth 1 step: couldn't solve 435710268,435760281,435706281 (solved 13)
-Depth 6 steps: solved until 435760281 (solved 14)
+Depth 7 steps: solved until 435760281 (solved 14)
 */
 void printPicture(int *pic){
 	for(int i=0; i<n; i++){
 		cout<<pic[i];
 		if((i+1)%d==0) cout<<endl;
+	}
+}
+
+void printPicture(int trace){
+	int pic[n];
+	for(int i=n-1; i>=0; i--){
+		pic[i] = trace%10;
+		trace /= 10;
+	}
+	printPicture(pic);
+}
+
+void getPicture(int* pic, int trace){
+	for(int i=n-1; i>=0; i--){
+		pic[i] = trace%10;
+		trace /= 10;
 	}
 }
 
@@ -34,7 +51,7 @@ int getKey(int *pic){
 
 int* tryStep(int *puzzle, int dir){
 	int i=0;
-	while(puzzle[i]!=0) i++;
+	while(puzzle[i]!=0) i++; //find piece 0
 	if(dir==-1 && (i+d)%d==0) return NULL; //piece 0 is first in row can't move backward
 	if(dir==1 && (i+1)%d==0) return NULL; //piece 0 is last in row can't move forward
 	if((i+dir)>=0 && (i+dir)<n){
@@ -51,8 +68,8 @@ int getCost(int *puzzle, int step){
 }
 
 int getMin(int a, int b){
-	if(a<0) return b;
-	if(b<0) return a;
+	if(a<=0) return b;
+	if(b<=0) return a;
 	return(a<b)?a:b;
 }
 
@@ -62,18 +79,25 @@ bool hadTrace(int key){
 	return(trace[i]!=key)?false:true;
 }
 
-int estimateCost(int *puzzle, int currentStep, int estimated, int maxSteps){
+//debugged for traces lost
+int estimateCost(int *puzzle, int currentStep, int lastStep, int estimated, int maxSteps){
 	int c[] = {0,0,0,0};
 	for(int i=0; i<4; i++){
-		if(tryStep(puzzle, moves[i])!=NULL){
-		//if step is valid and is not in traces
+		//if step is valid and is not in traces (avoid moving backward from last move)
+		if(currentStep + lastStep != 0 && tryStep(puzzle, moves[i])!=NULL){
 			if(!hadTrace(getKey(puzzle))){
 				//add trace
 				currentTrace += 1;
 				trace[currentTrace] = getKey(puzzle);
-				//estimate deeper steps
-				if(estimated<maxSteps) c[i] = getCost(puzzle, currentStep) 
-					+ estimateCost(puzzle, currentStep+1, estimated+1, maxSteps);
+				if(getCost(puzzle, currentStep)==currentStep){
+					trace[currentTrace] = 0;
+					currentTrace -= 1;
+					tryStep(puzzle, -moves[i]); //undo step
+					return 0; //if puzzle is solved
+				}
+				//deeper steps
+				if(estimated<maxSteps) c[i] = getCost(puzzle, currentStep) +
+					estimateCost(puzzle, currentStep+1, currentStep, estimated+1, maxSteps);
 				//remove trace
 				trace[currentTrace] = 0;
 				currentTrace -= 1;
@@ -91,13 +115,14 @@ void solvePuzzle(int* puzzle){
 	while(getKey(puzzle)!=picKey && currentTrace<MAX &&
 			trace[currentTrace] != trace[currentTrace-1]){ //stop if traces duplicated
 		for(int i=0; i<4; i++){
+			//try possible steps to find min cost step
 			if(tryStep(puzzle, moves[i])!= NULL){
 				if(!hadTrace(getKey(puzzle))){
 					//add trace
 					currentTrace += 1;
 					trace[currentTrace] = getKey(puzzle);
 					//use 0 for max steps of estimateCost for depth = 1;
-					costs[i] = getCost(puzzle,step)+estimateCost(puzzle,step+1,1,6);
+					costs[i] = getCost(puzzle,step)+estimateCost(puzzle,step+1, step,1,6);
 					//remove trace
 					trace[currentTrace] = 0;
 					currentTrace -= 1;
@@ -110,13 +135,13 @@ void solvePuzzle(int* puzzle){
 		}
 		int min = getMin(costs[0],getMin(costs[1],getMin(costs[2],costs[3])));
 		int i = 0;
-		while(i<4 && min != costs[i]) i++;
+		while(i<4 && min != costs[i]) i++; //find min cost step
 		tryStep(puzzle, moves[i]); //perform min cost step
+		printPicture(puzzle);
+		cout<<"----"<<endl;
 		step += 1;
 		currentTrace += 1;
 		trace[currentTrace] = getKey(puzzle); //add trace
-		printPicture(puzzle);
-		cout<<"----"<<endl;
 	}
 	if(getKey(puzzle)==getKey(picture)) cout<<"Solved after "<<step<<" moves";
 	else cout<<"Could not solve after "<<step<<" moves";
